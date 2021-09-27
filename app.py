d9568e6d67d6b12c88f8e0e6b19d37208b70e69f
Jesse Hill <jesse.o.hill@gmail.com>
2021-09-26 23:53:04 -0400

    implemented Step Two

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 """Blogly application."""
 
-import re
 from flask import Flask, render_template, redirect, request
-from flask.wrappers import Request
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
+from datetime import datetime
+
 
 app = Flask(__name__)
 app.debug = True
@@ -99,10 +99,79 @@ def edit_user(user_id):
 def delete_user(user_id):
     '''Deletes a user from the database and redirects to the user list.'''
     
-    
     user = User.query.filter_by(id=user_id)
     user.delete()
     
     db.session.commit()
     
     return redirect('/users')
+
+
+@app.route('/users/<int:user_id>/posts/new', methods=['GET', 'POST'])
+def new_post(user_id):
+    '''Displays new post form for a specific user.'''
+    
+    if request.method == 'GET':
+    
+        user = User.query.get(user_id)
+    
+        return render_template('new-post.html', user=user)
+    
+    if request.method == 'POST':
+        
+        # create a class instance using the form data
+        title = request.form['title']
+        content = request.form['content']
+        created_at = datetime.now()
+        
+        new_post = Post(title=title, content=content, created_at=created_at, owning_user=user_id)
+        
+        db.session.add(new_post)
+        db.session.commit()
+        
+        return redirect(f'/users/{user_id}')
+    
+    
+@app.route('/posts/<int:post_id>')
+def show_post(post_id):
+    '''Displays an individual post's details.'''
+    
+    post = Post.query.get(post_id)
+    
+    return render_template('post-details.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit', methods=['GET', 'POST'])
+def edit_post(post_id):
+    '''Shows a form to edit a post.'''
+    
+    if request.method == 'GET':
+        
+        post = Post.query.get(post_id)
+        
+        return render_template('edit-post.html', post=post)
+    
+    if request.method == 'POST':
+        
+        title = request.form['title']
+        content = request.form['content']
+        
+        updated_post = Post.query.get(post_id)
+        updated_post.title = title
+        updated_post.content = content
+        
+        db.session.commit()
+        
+        return redirect(f'/posts/{post_id}')
+    
+    
+@app.route('/posts/<int:post_id>/delete', methods=['POST'])
+def delete_post(post_id):
+    '''Deletes a post and redirects back to a user screen.'''
+    
+    post = Post.query.filter_by(id=post_id)
+    post.delete()
+    
+    db.session.commit()
+    
+    return redirect(f'/users')
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 """Models for Blogly."""
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import backref
 
 db = SQLAlchemy()
 
@@ -17,11 +18,26 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-
-    first_name = db.Column(db.String(), nullable=False)
-
-    last_name = db.Column(db.String(), nullable=False)
-
-    image_url = db.Column(db.String(), default='/assets/blank_photo.jpg')
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    image_url = db.Column(db.Text, default='/assets/blank_photo.jpg')
     
+    def fullname(self):
+        return f'{self.first_name} {self.last_name}'
     
+
+class Post(db.Model):
+    '''Post model to describe post details.'''
+    
+    __tablename__ = 'posts'
+    
+    def __repr__(self):
+        return f'<Post title={self.title} content={self.content} user={self.owning_user}>'
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(50), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    owning_user = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    user = db.relationship('User', backref='posts')
